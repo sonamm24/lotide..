55e810d1aeb7f619e4f5ed8df32f4f0a98e38541
Sonam <sonam.malhotra2496@gmail.com>
2021-07-25 01:05:52 +0000

    Find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}] `);
+  }
+  
+};
+
+
+
+
+const findKeyByValue =  function(object, value) {
+  for (const key in object) {
+    console.log(`${key}: ${object[key]}`);
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+      
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+      
