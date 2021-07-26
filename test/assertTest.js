8e03f4de2b4b8390b2e3885f51bafda31d11f916
Sonam <sonam.malhotra2496@gmail.com>
2021-07-26 20:12:28 +0000

    Refactoring assertarrayequal

---
+++ test/assertTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual =  require ('../assertArraysEqual');
+const assertEqual = require ('../assertEqual');
+
+
+
+assertArraysEqual("Lighthouse Labs", "Lighthouse Labs");
+assertArraysEqual([1, 5], [1, 5]);
